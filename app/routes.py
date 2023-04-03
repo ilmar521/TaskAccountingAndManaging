b61a637c86409b62a37ea49a1df48039073cc4c2
ilmar521 <ilmar521@mail.ru>
2023-04-03 16:18:05 +0300

    Commit

--- app/routes.py
+++ app/routes.py
@@ -8,14 +8,18 @@ from flask import flash
 @flask_app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        project = request.form.getlist('project')
-        status = request.form.getlist('status')
+        projects = request.form.getlist('project')
+        statuses = request.form.getlist('status')
+        current_project = int(projects[0])
+        current_status = statuses[0]
         all_projects = Project.query.all()
-        all_tasks = list(Task.query.filter(Task.project_id == project[0], Task.status == status[0]))
+        all_tasks = list(Task.query.filter(Task.project_id == current_project, Task.status == current_status))
     else:
         all_projects = Project.query.all()
-        all_tasks = list(Task.query.filter(Task.project == all_projects[0], Task.status == 'new'))
-    return flask.render_template('index.html', all_tasks=all_tasks, all_projects=all_projects)
+        current_project = all_projects[0]
+        current_status = 'new'
+        all_tasks = list(Task.query.filter(Task.project == current_project, Task.status == current_status))
+    return flask.render_template('index.html', all_tasks=all_tasks, all_projects=all_projects, current_project=current_project, current_status=current_status)
 #
 #
 # @flask_app.route("/complete/<int:todo_id>")
