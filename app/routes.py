2bbfbab8b46082be0b481ca0845be4742c180b9a
ilmar521 <ilmar521@mail.ru>
2023-04-03 13:52:01 +0300

    Commit

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,21 @@
 import flask
+from flask import request
 from app import flask_app, db
-# from app.models import Todo
+from app.models import Task, Project
 from flask import flash
 
 
-# @flask_app.route("/", methods=("GET", "POST"))
-# def index():
-#     if flask.request.method == "POST":
-#         task_name = flask.request.form.get("task-name")
-#         if task_name == '':
-#             flash('Enter the name of task!')
-#         else:
-#             new_task = Todo(details=task_name)
-#             new_task.save_task_to_db()
-#     all_tasks = Todo.query.all()
-#     return flask.render_template('index.html', all_tasks=all_tasks)
+@flask_app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        project = request.form.getlist('project')
+        status = request.form.getlist('status')
+        all_projects = Project.query.all()
+        all_tasks = list(Task.query.filter(Task.project_id == project[0], Task.status == status[0]))
+    else:
+        all_projects = Project.query.all()
+        all_tasks = list(Task.query.filter(Task.project == all_projects[0], Task.status == 'new'))
+    return flask.render_template('index.html', all_tasks=all_tasks, all_projects=all_projects)
 #
 #
 # @flask_app.route("/complete/<int:todo_id>")
